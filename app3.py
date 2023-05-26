3fa6492dd1eb19f27677d63c127a35c1330eb9d6
reyvanmrm <55115205+reyvanmrm@users.noreply.github.com>
2023-05-26 09:23:09 +0200

    Update app3.py

--- app3.py
+++ app3.py
@@ -18,32 +18,25 @@ with st.sidebar:
     st.title('LLM Chat App')
     st.markdown('''
     ## About
-    Upload your PDF to talk
+    Upload your PDFs to talk
     ''')
 
 load_dotenv()
 
 def main():
-    st.header("Chat with PDF 💬")
+    st.header("Chat with PDFs 💬")
 
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-    pdf = st.file_uploader("Upload your PDF", type='pdf')
-    pdf_url = st.text_input("Or, enter PDF URL:")
+    pdfs = st.file_uploader("Upload your PDFs", type='pdf', accept_multiple_files=True)
+    text = ""
 
-    if pdf is not None:
-        pdf_reader = PdfReader(pdf)
-    elif pdf_url != "":
-        pdf_data = requests.get(pdf_url)
-        pdf_file = tempfile.NamedTemporaryFile(delete=False)
-        pdf_file.write(pdf_data.content)
-        pdf_file.close()
-        pdf_reader = PdfReader(pdf_file.name)
-
-    if 'pdf_reader' in locals():  # Make sure PDF has been uploaded or downloaded
-        text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
+    if pdfs is not None:
+        for pdf in pdfs:
+            pdf_reader = PdfReader(pdf)
+            
+            for page in pdf_reader.pages:
+                text += page.extract_text()
 
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=1000,
@@ -52,7 +45,7 @@ def main():
         )
         chunks = text_splitter.split_text(text=text)
 
-        store_name = pdf.name[:-4] if pdf is not None else pdf_url.split('/')[-1].split('.')[0]
+        store_name = 'multiple_pdfs'
         st.write(f'{store_name}')
 
         if os.path.exists(f"{store_name}.pkl"):
@@ -64,17 +57,19 @@ def main():
             with open(f"{store_name}.pkl", "wb") as f:
                 pickle.dump(VectorStore, f)
 
-        query = st.text_input("Ask questions about your PDF file:")
+        queries = st.text_input("Ask questions about your PDF files (separate questions with a semicolon):")
+        queries = [query.strip() for query in queries.split(';')]
 
-        if query:
-            docs = VectorStore.similarity_search(query=query, k=3)
+        for query in queries:
+            if query:
+                docs = VectorStore.similarity_search(query=query, k=3)
 
-            llm = OpenAI(model_name='text-davinci-003', api_key=OPENAI_API_KEY)
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
-            with get_openai_callback() as cb:
-                response = chain.run(input_documents=docs, question=query)
-                print(cb)
-            st.write(response)
+                llm = OpenAI(model_name='text-davinci-003', api_key=OPENAI_API_KEY)
+                chain = load_qa_chain(llm=llm, chain_type="stuff")
+                with get_openai_callback() as cb:
+                    response = chain.run(input_documents=docs, question=query)
+                    print(cb)
+                st.write(response)
 
 if __name__ == '__main__':
     main()
