1b39be7dfd8a5a637ab6fbde4ddb1739c0b183d7
reyvanmrm <55115205+reyvanmrm@users.noreply.github.com>
2023-05-26 09:38:20 +0200

    Update app3.py

--- app3.py
+++ app3.py
@@ -28,6 +28,9 @@ def main():
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
     pdfs = st.file_uploader("Upload your PDFs", type='pdf', accept_multiple_files=True)
+    pdf_urls = st.text_input("Or, enter PDF URLs (separate URLs with a comma):")
+    pdf_urls = [url.strip() for url in pdf_urls.split(',') if url.strip()]
+
     text = ""
 
     if pdfs is not None:
@@ -37,42 +40,54 @@ def main():
             for page in pdf_reader.pages:
                 text += page.extract_text()
 
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len
-        )
-        chunks = text_splitter.split_text(text=text)
+    if pdf_urls:
+        for pdf_url in pdf_urls:
+            pdf_data = requests.get(pdf_url)
+            pdf_file = tempfile.NamedTemporaryFile(delete=False)
+            pdf_file.write(pdf_data.content)
+            pdf_file.close()
+            pdf_reader = PdfReader(pdf_file.name)
+            
+            for page in pdf_reader.pages:
+                text += page.extract_text()
 
-        if not chunks:
-            st.warning('No text extracted from the uploaded files. Please upload valid non-empty PDFs.')
-            return
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text=text)
 
-        store_name = 'multiple_pdfs'
-        st.write(f'{store_name}')
+    if not chunks:
+        st.warning('No text extracted from the uploaded files. Please upload valid non-empty PDFs.')
+        return
 
-        if os.path.exists(f"{store_name}.pkl"):
-            with open(f"{store_name}.pkl", "rb") as f:
-                VectorStore = pickle.load(f)
-        else:
-            embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-            with open(f"{store_name}.pkl", "wb") as f:
-                pickle.dump(VectorStore, f)
+    store_name = 'multiple_pdfs'
+    st.write(f'{store_name}')
 
-        queries = st.text_input("Ask questions about your PDF files (separate questions with a semicolon):")
-        queries = [query.strip() for query in queries.split(';')]
+    if os.path.exists(f"{store_name}.pkl"):
+        with open(f"{store_name}.pkl", "rb") as f:
+            VectorStore = pickle.load(f)
+    else:
+        embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+        VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+        with open(f"{store_name}.pkl", "wb") as f:
+            pickle.dump(VectorStore, f)
 
-        for query in queries:
-            if query:
-                docs = VectorStore.similarity_search(query=query, k=3)
+    queries = st.text_input("Ask questions about your PDF files (separate questions with a semicolon):")
+    queries = [query.strip() for query in queries.split(';')]
 
-                llm = OpenAI(model_name='text-davinci-003', api_key=OPENAI_API_KEY)
-                chain = load_qa_chain(llm=llm, chain_type="stuff")
-                with get_openai_callback() as cb:
-                    response = chain.run(input_documents=docs, question=query)
-                    print(cb)
-                st.write(response)
+    for query in queries:
+        if query:
+            docs = VectorStore.similarity_search(query=query, k=3)
+
+            llm = OpenAI(model_name='text-davinci-003', api_key=OPENAI_API_KEY)
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=query)
+                print(cb)
+            st.write(response)
 
 if __name__ == '__main__':
     main()
+
